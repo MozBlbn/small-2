d180f7dc1011f6d5f5af66ce23412dc72d541b3b
MozBlbn <165378477+MozBlbn@users.noreply.github.com>
2024-09-03 16:26:42 +0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -7,14 +7,14 @@ REPO2_URL="https://github.com/MozBlbn/small-1.git"
 REPO3_URL="https://github.com/MozBlbn/big.git"
 
 # Create the directory
-mkdir -p "$DIR_NAME"
+# mkdir -p "$DIR_NAME"
 
 # Change to the newly created directory
-cd "$DIR_NAME"
+# cd "$DIR_NAME"
 
 # Clone the repositories
-git clone "$REPO1_URL"
-git clone "$REPO2_URL"
-git clone "$REPO3_URL"
+# git clone "$REPO1_URL"
+# git clone "$REPO2_URL"
+# git clone "$REPO3_URL"
 
 echo "Repositories have been cloned into $DIR_NAME."
